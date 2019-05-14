a1036a90bec3baa38e55107ef2b3b2df44e73d59
jointtask <jointtask.ng@gmail.com>
2019-05-14 09:34:09 +0100

    feature<update>:(ui) -> change app name to EDSUF.

--- lib/routes/login/login.dart
+++ lib/routes/login/login.dart
@@ -88,7 +88,7 @@ class _LoginRouteState extends State<LoginRoute>
                       style: TextStyle(color: Colors.teal, fontSize: 40),
                     ),
                     Text(
-                      'Suf',
+                      'SUF',
                       style: TextStyle(
                           color: Colors.amber,
                           fontSize: 35,
