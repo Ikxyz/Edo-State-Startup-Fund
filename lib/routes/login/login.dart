2e3ca6210fa7bf20ea0902aed6f511f0bd5d83b6
jointtask <jointtask.ng@gmail.com>
2019-05-14 06:30:35 +0100

    featuer<update>:(ui)=> update app name

--- lib/routes/login/login.dart
+++ lib/routes/login/login.dart
@@ -84,11 +84,11 @@ class _LoginRouteState extends State<LoginRoute>
                 child: Row(
                   children: <Widget>[
                     Text(
-                      'EDS',
+                      'ED',
                       style: TextStyle(color: Colors.teal, fontSize: 40),
                     ),
                     Text(
-                      'Funds',
+                      'Suf',
                       style: TextStyle(
                           color: Colors.amber,
                           fontSize: 35,
